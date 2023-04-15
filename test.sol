9a0491abe4479234a1a5f7afaf430845b02315e4
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-04-15 12:13:17 +0900

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+error notOwner();
+
 contract Bank {
+
+    address public immutable Owner;
+    uint256 public test;
+
+    constructor() {
+        Owner = msg.sender;
+    }
 
     mapping(address => uint256) public balanceOf;   // balances, indexed by addresses
 
@@ -12,5 +21,10 @@ contract Bank {
     function withdraw(uint256 amount) public {
         balanceOf[msg.sender] -= amount; //Will revert with type uint256 with Solidity version 0.8.0.
         payable(msg.sender).transfer(amount);
+    }
+
+    function ownerTest() public {
+        if(msg.sender != Owner ) {revert notOwner(); }
+        test = block.timestamp;
     }
 }
