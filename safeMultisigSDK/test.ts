def52b872c47f4c18214402fabcfaacd5f335df9
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2023-04-16 02:32:13 +0900

    Update test.ts

--- safeMultisigSDK/test.ts
+++ safeMultisigSDK/test.ts
@@ -1,6 +1,6 @@
 // NEED TO HAVE "package.json" file with "type": "module" set for import syntax.
 import { ethers } from 'ethers'
-import { EthersAdapter } from '@safe-global/protocol-kit'
+import { EthersAdapter } from '@safe-global/protocol-kit'
 import { SafeFactory } from '@safe-global/protocol-kit'
   
 const RPC_URL='https://eth-goerli.public.blastapi.io'
@@ -8,7 +8,8 @@ const RPC_URL='https://eth-goerli.public.blastapi.io'
 const safeAddress = '0x9F4Fc2673c4542F0cC4C5ebeEB24ebB19F8BE29f';
 const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
 
-const owner1Signer = new ethers.Wallet(process.env.devTestnetPrivateKey, provider)
+const privateKey = "0x" + process.env.devTestnetPrivateKey
+const owner1Signer = new ethers.Wallet(privateKey, provider)
       
 const ethAdapterOwner1 = new EthersAdapter({
   ethers,
